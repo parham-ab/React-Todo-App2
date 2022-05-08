2d2128ff79412b37be0f01ead785ad0365594b06
Parham Abolghasemi <parhamab17@gmail.com>
2022-05-08 10:52:58 +0430

    edit todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 // icons
 import { MdModeEditOutline } from "react-icons/md";
 import { TiDelete } from "react-icons/ti";
+import TodoForm from "./TodoForm";
 
-const Todo = ({ todos, deleteHandler, id }) => {
+const Todo = ({ todos, deleteHandler, id, updateTodo }) => {
+  // edit todo
+  const [edit, setEdit] = useState({ id: null, value: "" });
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
   return todos.map((todo, index) => (
     <div key={index}>
       <div key={todo.id}>{todo.text}</div>
-      <MdModeEditOutline />
+      <MdModeEditOutline
+        onClick={() => setEdit({ id: todo.id, value: todo.text })}
+      />
       <TiDelete onClick={() => deleteHandler(todo.id)} />
     </div>
   ));
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,25 +5,40 @@ import Todo from "./Todo";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  // add new todo
+  // Add new todo
   const addTodo = (todo) => {
-    //   prevent from adding empty text
+    //   Prevent from adding empty text
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
     const newTodo = [...todos, todo];
     setTodos(newTodo);
   };
-  // delete a todo from the list
+  // Delete a todo from the list
   const deleteHandler = (id) => {
     const newValue = [...todos].filter((item) => item.id !== id);
     setTodos(newValue);
   };
+  // Edit todo
+  const updateTodo = (todoId, todoValue) => {
+    //   Prevent from adding empty text
+    if (!todoValue.text || /^\s*$/.test(todoValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? todoValue : item))
+    );
+  };
+
   return (
     <div>
       <h1>React Todo App</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} deleteHandler={deleteHandler} />
+      <Todo
+        todos={todos}
+        deleteHandler={deleteHandler}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 };
