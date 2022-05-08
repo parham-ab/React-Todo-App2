1a14a673436df83f3481044506f93af6f1475f27
Parham Abolghasemi <parhamab17@gmail.com>
2022-05-08 10:03:59 +0430

    todo form update, input auto focus onMount, clear input after submitting

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+// icons
+import { SiAddthis } from "react-icons/si";
+// import { MdCloudDone } from "react-icons/md";
 // components
 import TodoList from "./TodoList";
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+  const [input, setInput] = useState("");
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  // submitHandler
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // clear input
+    setInput("");
+  };
+  // changeHandler
+  const changeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <div>asas
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="text"
+          placeholder="Add Todo..."
+          value={input}
+          onChange={changeHandler}
+          ref={inputRef}
+        />
+        <button>
+          <SiAddthis />
+        </button>
+      </form>
       <TodoList />
     </div>
   );
