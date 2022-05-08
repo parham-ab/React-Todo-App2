2bfde6c9d3521caa52938625ec6ad9d19a256d4a
Parham Abolghasemi <parhamab17@gmail.com>
2022-05-08 10:59:08 +0430

    bugs fixed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 // icons
 import { SiAddthis } from "react-icons/si";
+import { MdCloudDone } from "react-icons/md";
 
 const TodoForm = (props) => {
   const [input, setInput] = useState("");
@@ -27,19 +28,35 @@ const TodoForm = (props) => {
 
   return (
     <form onSubmit={submitHandler}>
-      <div>
-        <input
-          type="text"
-          name="text"
-          placeholder="Add Todo..."
-          value={input}
-          onChange={changeHandler}
-          ref={inputRef}
-        />
-        <button>
-          <SiAddthis />
-        </button>
-      </div>
+      {!props.edit ? (
+        <div>
+          <input
+            type="text"
+            name="text"
+            placeholder="Add Todo..."
+            value={input}
+            onChange={changeHandler}
+            ref={inputRef}
+          />
+          <button>
+            <SiAddthis />
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            name="text"
+            placeholder="Update Todo..."
+            value={input}
+            onChange={changeHandler}
+            ref={inputRef}
+          />
+          <button>
+            <MdCloudDone />
+          </button>
+        </div>
+      )}
     </form>
   );
 };
